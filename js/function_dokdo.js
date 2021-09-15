7b06b78c8317d70ed520cb4abbae311660b942cf
ZetGar <88043986+ZetGar@users.noreply.github.com>
2021-09-15 16:36:04 +0900

    Update dokdo.css

--- js/function_dokdo.js
+++ js/function_dokdo.js
@@ -13,12 +13,8 @@ $(function(){
       $main.removeClass('sticky');
 
     }
-
-
   });
   
-
-
 
   // login 모달창 열기
   const $login= $('.login');
@@ -70,105 +66,43 @@ $(function(){
 
 
 
-  // 메뉴 위치변경
-  const arrTopVal = [];
-  const $mnu = $('.gnb>li>a');
+//   // 메뉴 위치변경
+//   const arrTopVal = [];
+//   const $mnu = $('.gnb>li>a');
 
-  for(let i=0;i<6;i++){
-    arrTopVal[i] = $('section').eq(i).offset().top;
-  }
+//   for(let i=0;i<6;i++){
+//     arrTopVal[i] = $('section').eq(i).offset().top;
+//   }
   
-  const moveFn = function(idx){
-    $('html,body').stop().animate({
-      scrollTop:arrTopVal[idx+2]},
-      'easeInOutCubic');
- }
+//   const moveFn = function(idx){
+//     $('html,body').stop().animate({
+//       scrollTop:arrTopVal[idx+2]},
+//       'easeInOutCubic');
+//  }
 
-    $mnu.on('click',(evt)=>{
-      evt.preventDefault();
+//     $mnu.on('click',(evt)=>{
+//       evt.preventDefault();
   
-      const nowIdx = $mnu.index(evt.currentTarget);
-      moveFn(nowIdx);
-    });
+//       const nowIdx = $mnu.index(evt.currentTarget);
+//       moveFn(nowIdx);
+//     });
 
-    $(window).on('scroll',function(){
+//     $(window).on('scroll',function(){
 
-      const scrollTop = $(window).scrollTop();
-      console.log(scrollTop);
+//       const scrollTop = $(window).scrollTop();
+//       console.log(scrollTop);
     
-        for(let i=0;i<6;i++){
+//         for(let i=0;i<6;i++){
      
-        if(scrollTop>=arrTopVal[i]-200){
-          $mnu.eq(i).parent().addClass('on').siblings().removeClass('on');
-        } else if(scrollTop<arrTopVal[1]){
-          $mnu.parent().removeClass('on');
-        }
-      }
+//         if(scrollTop>=arrTopVal[i]-200){
+//           $mnu.eq(i).parent().addClass('on').siblings().removeClass('on');
+//         } else if(scrollTop<arrTopVal[1]){
+//           $mnu.parent().removeClass('on');
+//         }
+//       }
 
-    });
-  // 독도메인비주얼페이지
-  const $mnVisual = $('.main_visual');
-  const $star1 = $('section.main #star1');
-  const $star2 = $('section.main #star2');
-  const $sun = $('section.main #sun');
-  const $dokdo = $('section.main #dokdo');
-  const $cloude = $('section.main .cloude');
-  const $mnBird = $('section.main .mn_bird');
-
-  $(window).on('scroll',function(){
-    const value = $(window).scrollTop();
-
-    // 이미지 변화
-    $star1.css({left : value*0.25 + 'px',
-  	opacity: '0',
-  animation: 'none'});
-    $star2.css({left : value*0.25 + 'px',
-  	opacity: '0',
-  animation: 'none'});
-
-    $sun.css({
-      bottom: value*3 + 'px',
-      backgroundColor:'#0b6fb3',
-      opacity:'1'
-  });
-
-  $cloude.css({
-    opacity:'0.7'
-  });
-
-  $mnBird.css({
-    bottom: 300 + 'px',
-    opacity:'1'
-});
-
-    $dokdo.css({top: value*0 + 'px'});
-
-    $mnVisual.css({
-      backgroundColor:'#0b6fb3'
-    });
-
-  });
-   
-
-  // 독도의 자연
-  const $naPagination = $('section.nature > .na_gnb > li>a');
-  const $naContent = $('.na_box');
-  const $naContext= $('.nabox>dl');
-
-  let naIdx=0;
-
-  $naPagination.on('click', function(evt){
-    evt.preventDefault();
-
-    naIdx = $naPagination.index(this);
-
-    // 활성화표시
-    $(this).eq(naIdx).parent().addClass('on').siblings().removeClass('on');
-
-    $naContent.eq(naIdx).addClass('on').siblings().removeClass('on');
-
-  });
-
+//     });
+  
   // 독도의 구성
   const $comBox = $('.com_box > .com_boxcontainer');
   const $plusX = $('section.component .plusX');
@@ -194,8 +128,5 @@ $(function(){
       animationFillMode: 'forwards'
     });
   }
-    
   });
-
-
 });
